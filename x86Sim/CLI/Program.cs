1e9dd49144ddb4a175c99bae8c2f4c51fe9320d0
Toqelle <toqelle@gmail.com>
2021-06-10 17:31:53 +0200

    Init

---
+++ x86Sim/CLI/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using x86;
+
+namespace CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CPU CPU = new CPU();
+
+            CPU.Registers.SaveRegisterValue(1, 0x01, 0x0001);
+
+            CPU.Instructions.MOV(1, 1, 0x02, 0x01);
+            Console.WriteLine(CPU.Registers.BX);
+
+
+        }
+    }
+}
